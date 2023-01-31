acdcf9cf438d49506d526677b42aff72498dece7
Chuck Kiefriter <ckiefriter@avidxchange.com>
2023-01-30 23:48:04 -0500

    Updated to match homework assignment

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@ public class Main {
 	
 		public static void main(String[] args) {
 			
-			System.out.println("Hello");
+			// create double variables and assign values.
+			double value1 = 14.99;
+			double value2 = 15.99;
+			
+			System.out.println(value1 + value2);
 			
 		}
 
